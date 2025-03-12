27841de91c1377f5b2ed91499093ef20fbe6c935
Zhe Sun <31067185+ZheSun88@users.noreply.github.com>
2025-03-12 15:04:54 +0200

    update viewport resize call (#1413)
    * update viewport resize call
    
    * Update AbstractIT.java

--- src/test/java/com/vaadin/starter/bakery/testbench/AbstractIT.java
+++ src/test/java/com/vaadin/starter/bakery/testbench/AbstractIT.java
@@ -1,6 +1,7 @@
 package com.vaadin.starter.bakery.testbench;
 
 import org.junit.Rule;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 
 import com.vaadin.starter.bakery.testbench.elements.ui.LoginViewElement;
@@ -25,7 +26,7 @@ public abstract class AbstractIT<E extends TestBenchElement> extends ParallelTes
 	@Override
 	public void setup() throws Exception {
 		super.setup();
-		testBench().resizeViewPortTo(1024, 800);
+                driver.manage().window().setSize(new Dimension(1024, 800));
 		if (getRunLocallyBrowser() == null) {
 			APP_URL = "http://" + IPAddress.findSiteLocalAddress() + ":8080/";
 		}
