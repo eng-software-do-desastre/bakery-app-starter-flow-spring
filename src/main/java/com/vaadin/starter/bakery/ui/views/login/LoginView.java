c125a16cbbd30538f103dca6b8ff3ea943bf70c7
Manolo Carrasco <manolo@vaadin.com>
2025-03-17 13:13:09 +0100

    Add more translations

--- src/main/java/com/vaadin/starter/bakery/ui/views/login/LoginView.java
+++ src/main/java/com/vaadin/starter/bakery/ui/views/login/LoginView.java
@@ -2,6 +2,7 @@ package com.vaadin.starter.bakery.ui.views.login;
 
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
+import static com.vaadin.flow.i18n.I18NProvider.translate;
 import com.vaadin.flow.router.AfterNavigationEvent;
 import com.vaadin.flow.router.AfterNavigationObserver;
 import com.vaadin.flow.router.BeforeEnterEvent;
@@ -19,15 +20,15 @@ public class LoginView extends LoginOverlay
 	public LoginView() {
 		LoginI18n i18n = LoginI18n.createDefault();
 		i18n.setHeader(new LoginI18n.Header());
-		i18n.getHeader().setTitle("###Bakery###");
+		i18n.getHeader().setTitle(translate("app.title"));
 		i18n.getHeader().setDescription(
 			"admin@vaadin.com + admin\n" + "barista@vaadin.com + barista");
 		i18n.setAdditionalInformation(null);
 		i18n.setForm(new LoginI18n.Form());
-		i18n.getForm().setSubmit("Sign in");
-		i18n.getForm().setTitle("Sign in");
-		i18n.getForm().setUsername("Email");
-		i18n.getForm().setPassword("Password");
+		i18n.getForm().setSubmit(translate("signin"));
+		i18n.getForm().setTitle(translate("login"));
+		i18n.getForm().setUsername(translate("email"));
+		i18n.getForm().setPassword(translate("password"));
 		setI18n(i18n);
 		setForgotPasswordButtonVisible(false);
 		setAction("login");
