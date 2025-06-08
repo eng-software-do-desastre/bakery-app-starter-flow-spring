f6e4aef1f00a600e267bdb992df2559a7384d3af
Manolo Carrasco <manolo@vaadin.com>
2025-06-08 20:01:26 +0200

    deploy both versions of the app

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -1,9 +1,17 @@
 
 
-## Build the application for control-center in production mode
-mvn clean package -DskipTests -P production -P control-center
 
 ## You need to be logged in with your user, otherwise run `docker login`
 USER=k8sdemos
-docker build --tag $USER/bakery-cc:next .
-docker push $USER/bakery-cc:next
+VERS=${VERS:-24.7}
+
+## Build the application for control-center in production mode
+mvn clean package -DskipTests -P production -P control-center
+docker build --tag $USER/bakery-cc:$VERS .
+docker push $USER/bakery-cc:$VERS
+
+
+## Build the application for control-center in production mode
+mvn clean package -DskipTests -P production
+docker build --tag $USER/bakery:$VERS .
+docker push $USER/bakery:$VERS
