d5d40cf10042b654e911d0bd1fbc41106c5a9d6e
Manolo Carrasco <manolo@vaadin.com>
2025-07-15 10:22:01 +0200

    set default version to 24.8

--- deploy_docker.sh
+++ deploy_docker.sh
@@ -3,7 +3,7 @@
 
 ## You need to be logged in with your user, otherwise run `docker login`
 USER=k8sdemos
-VERS=${VERS:-24.7}
+VERS=${VERS:-24.8}
 
 ## Build the application for control-center in production mode
 mvn clean package -DskipTests -P production -P control-center
